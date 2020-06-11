da72667085287968e0b97d9f4f13ac1e704ec92e
Danshil Mungur <danshil.mungur@gmail.com>
2020-06-12 01:24:15 +0400

    removed duplicate prompts and fixed file permissions

--- Services/Shared/Radarr/TmmToRadarr/script.sh
+++ Services/Shared/Radarr/TmmToRadarr/script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 cd /home/user/Services/Stack
-echo "Stopping radarr.."
 docker-compose stop radarr
-cp /home/user/Services/Radarr/nzbdrone.db /home/user/Services/Shared/Radarr/TmmToRadarr/
+cp -p /home/user/Services/Radarr/nzbdrone.db /home/user/Services/Shared/Radarr/TmmToRadarr/
 cd /home/user/Services/Shared/Radarr/TmmToRadarr/
 echo "Removing last empty line from movielist.csv..."
 sed -i '$d' Exported/movielist.csv
@@ -11,8 +10,7 @@ echo "Updating records in radarr db - check last_run.log for info"
 runuser -l user -c 'python3 /home/user/Services/Shared/Radarr/TmmToRadarr/updateRadarrDBEntries.py >> /home/user/Services/Shared/Radarr/TmmToRadarr/last_run.log'
 echo "Copying back to radarr config directory and making backup of current db.."
 rm /home/danshil/Services/Radarr/nzbdrone.db*
-cp nzbdrone.db /home/user/Services/Radarr/
+cp -p nzbdrone.db /home/user/Services/Radarr/
 mv --backup=numbered nzbdrone.db Backups/
 cd /home/danshil/Services/Stack
-echo "Starting radarr..."
 docker-compose up -d --force-recreate radarr
