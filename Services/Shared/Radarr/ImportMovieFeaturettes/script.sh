0eef4dc38b5993a03aaa9da92a2a3822f51be6bb
Danshil Mungur <danshil.mungur@gmail.com>
2020-11-06 00:03:07 +0400

    added testing condition to ImportMovieFeaturettes

--- Services/Shared/Radarr/ImportMovieFeaturettes/script.sh
+++ Services/Shared/Radarr/ImportMovieFeaturettes/script.sh
@@ -5,13 +5,19 @@ now=$(date +'%F %T')
 touch /shared/ImportMovieFeaturettes/lastrun.log
 logfile="/shared/ImportMovieFeaturettes/lastrun.log"
 
+if [[ $radarr_eventtype == "Test" ]] ; then
+	echo "$now: Test successful!" >> $logfile
+fi
+
 ## these folder names are based on what I've seen when grabbing movies from Torrents/Usenet, so add to the list if there's any mossing
-for featurettesFolderName in "Extras" "Behind the Scenes" "behindthescenes" "Deleted scenes" "deletedscenes" "Featurettes" "Interviews" "Scenes" "Shorts"
-do
-	if [ -d "$radarr_moviefile_sourcefolder/$featurettesFolderNames" ]; then
-		echo "$now: Found Featurette folder ($featurettesFolderName) in completed folder download location: $radarr_moviefile_sourcefolder" >> $logfile
-		echo "$now: Copying Featurette folder to movie path: $radarr_movie_path" >> $logfile
-		# change to move instead of copy?
-		cp -r "$radarr_moviefile_sourcefolder/$featurettesFolderName" "$radarr_movie_path/$featurettesFolderName"
-	fi
-done
+if [[ $radarr_eventtype == "Grab" ]] || [[ $radarr_eventtype == "Download" ]]; then
+	for featurettesFolderName in "Extras" "Behind the Scenes" "behindthescenes" "Deleted scenes" "deletedscenes" "Featurettes" "Interviews" "Scenes" "Shorts"
+	do
+		if [[ -d "$radarr_moviefile_sourcefolder/$featurettesFolderNames" ]]; then
+			echo "$now: Found Featurette folder ($featurettesFolderName) in completed folder download location: \"$radarr_moviefile_sourcefolder\"" >> $logfile
+			echo "$now: Copying Featurette folder to movie path: \"$radarr_movie_path\"" >> $logfile
+			# change to move instead of copy?
+			cp -r "$radarr_moviefile_sourcefolder/$featurettesFolderName" "$radarr_movie_path/$featurettesFolderName"
+		fi
+	done
+fi
